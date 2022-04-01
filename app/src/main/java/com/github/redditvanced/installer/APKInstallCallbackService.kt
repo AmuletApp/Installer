67583c37616a04d1ff58fce269dce17ed6d3df28
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-03-31 19:29:36 -0700

    feat: toasts upon install results

--- app/src/main/java/com/github/redditvanced/installer/APKInstallCallbackService.kt
+++ app/src/main/java/com/github/redditvanced/installer/APKInstallCallbackService.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageInstaller
 import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
 
 class APKInstallCallbackService : Service() {
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
@@ -15,11 +16,31 @@ class APKInstallCallbackService : Service() {
                 confirmationIntent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(confirmationIntent)
             }
-            PackageInstaller.STATUS_SUCCESS ->
+            PackageInstaller.STATUS_SUCCESS -> {
                 Log.i("Installer", "Installation succeeded")
-            else ->
+                Toast.makeText(
+                    this,
+                    "Successfully installed Amulet!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            PackageInstaller.STATUS_FAILURE_ABORTED -> {
+                Toast.makeText(
+                    this,
+                    "Aborted installation",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            else -> {
                 Log.i("Installer", "Installation failed: error code $statusCode")
+                Toast.makeText(
+                    this,
+                    "Failed: Error $statusCode",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
+
         stopSelf()
         return START_NOT_STICKY
     }
